b2fb959af0576f86720efb3d8efb68d41f17b533
Andrew-Goit <105640738+Andrew-Goit@users.noreply.github.com>
2023-01-20 10:16:39 +0200

    final

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -29,8 +29,6 @@ export const Form = () => {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        // value={name}
-        // onChange={handleChange}
       />
 
       <Label htmlFor="tel">Number</Label>
@@ -41,8 +39,6 @@ export const Form = () => {
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
-        // value={number}
-        // onChange={handleChange}
       />
 
       <SubmitBtn type="submit">Add contact</SubmitBtn>
